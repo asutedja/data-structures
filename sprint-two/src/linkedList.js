fb67f23c6820afdd19c6aca6a14a282973945a5b
asutedja <asutedja@gmail.com>
2016-07-28 10:55:10 -0700

    implement linkedList methods

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,60 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    
+    var node = Node(value);
+    if (list.tail === null) {
+      if (list.head === null) {
+        list.head = node; 
+        list.tail = node; 
+      } else {
+        list.tail.next = node;
+        list.tail = node;
+      }
+    } else if (list.head.next === null) {
+      list.tail = node;
+      list.head.next = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    //Find out where the head it at
+    var temp = list.head.next;
+    var result = list.head.value;
+    list.head.value = null;
+    list.head.next = null;
+    list.head = temp;
+    return result;
+    //Find the next node after current head
+    //Assign the next node to be the head
+    //Delete the former head
+
+
   };
 
   list.contains = function(target) {
+    
+    var check = list.head;
+    var result = false;
+    while ( check.next !== null && result === false) {
+      if (check.value === target) {
+        result = true;
+      } else {
+        check = check.next;
+      }
+    }
+    if (list.tail.value === target) {
+      result = true;
+    }
+    return result;
+    //Check each node value for target
+      //Check head first
+      //Check head.next
+      //Check until you find it or hit null
+
   };
 
   return list;
@@ -26,4 +74,5 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ How fast it iterates through the linkedlist.
  */
