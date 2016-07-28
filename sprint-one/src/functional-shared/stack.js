eedea45305c9e69638d1bab2d504a77c70bf2539
asutedja <asutedja@gmail.com>
2016-07-27 18:17:40 -0700

    Fix stack functional-shared pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,38 +1,50 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var storage = {};
-  var storageLength = 0;
+  var newStack = {};
+  newStack.length = 0;
+  extend(newStack, stackMethods);
+  return newStack;
 
-  var size = function() {
-    return storageLength;
-  };
 
-  var push = function(value) {
-    storage[storageLength] = value;
-    storageLength++;
-  };
+};
 
-  var pop = function() {
-    var last = storage[storageLength - 1];
-    delete storage[storageLength - 1];
-    storageLength--;
-    if (storageLength < 0) {
-      storageLength = 0;
-    }
-    return last;
-
-  };
-
-  stackMethods['size'] = size;
-  stackMethods['push'] = push;
-  stackMethods['pop'] = pop;
-  return stackMethods;
+var extend = function(to, here) {
+  for (var key in here) {
+    to[key] = here[key];
+  }
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  //Adding value to the back of stack
+  this[this.length] = value;
+  this.length++;
+};
 
-var test = Stack();
+stackMethods.pop = function() {
+  //Setting last to be last value of stack
+  var last = this[this.length - 1];
+  //Delete last value of stack
+  delete this[this.length - 1];
+  //Decreases stack length
+  this.length--;
+  //Ensures stack length doesn't go below 0
+  if (this.length < 0) {
+    this.length = 0;
+  }
+  //return last element;
+  return last;
+
+
+
+};
+
+stackMethods.size = function() {
+  return this.length;
+};
+
+
 
 
