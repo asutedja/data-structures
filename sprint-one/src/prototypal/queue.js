61a03df9e67f46f2dfa7e858bf6ef2b78fbee988
asutedja <asutedja@gmail.com>
2016-07-27 19:30:22 -0700

    Implement stack and queue prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.length = 0;
+  return newQueue;
+
 };
 
 var queueMethods = {};
+
+
+queueMethods.size = function() {
+  return this.length;
+};
+
+queueMethods.enqueue = function(value) {
+  this[this.length] = value;
+  this.length++;
+};
+
+
+queueMethods.dequeue = function() {
+  var first = this[0];
+  delete this[0];
+  for (var key in this) {
+    var newKey = key - 1;
+    if (newKey !== NaN) {
+      this[newKey] = this[key];
+    }
+  }
+  this.length--;
+  if (this.length < 0) {
+    this.length = 0;
+  }
+  
+  return first;
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,44 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.length = 0;
+  return newStack;
+
+
 };
 
+
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  //Adding value to the back of stack
+  this[this.length] = value;
+  this.length++;
+};
+
+stackMethods.pop = function() {
+  //Setting last to be last value of stack
+  var last = this[this.length - 1];
+  //Delete last value of stack
+  delete this[this.length - 1];
+  //Decreases stack length
+  this.length--;
+  //Ensures stack length doesn't go below 0
+  if (this.length < 0) {
+    this.length = 0;
+  }
+  //return last element;
+  return last;
+
+
+
+};
+
+stackMethods.size = function() {
+  return this.length;
+};
+
+
 
 
