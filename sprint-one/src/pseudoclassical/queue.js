82013f81d07e520a0053ebaafabb1de96bb49bf1
asutedja <asutedja@gmail.com>
2016-07-27 20:00:59 -0700

    Implement stack and queue pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+
+};
+
+
+Queue.prototype.size = function() {
+  return this.length;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this[this.length] = value;
+  this.length++;
+};
+
+
+Queue.prototype.dequeue = function() {
+  var first = this[0];
+  delete this[0];
+  for (var key in this) {
+    var newKey = key - 1;
+    if (newKey !== NaN) {
+      this[newKey] = this[key];
+    }
+  }
+  this.length--;
+  if (this.length < 0) {
+    this.length = 0;
+  }
+  
+  return first;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
 };
+
+
+var stackMethods = {};
+
+Stack.prototype.push = function(value) {
+  //Adding value to the back of stack
+  this[this.length] = value;
+  this.length++;
+};
+
+Stack.prototype.pop = function() {
+  //Setting last to be last value of stack
+  var last = this[this.length - 1];
+  //Delete last value of stack
+  delete this[this.length - 1];
+  //Decreases stack length
+  this.length--;
+  //Ensures stack length doesn't go below 0
+  if (this.length < 0) {
+    this.length = 0;
+  }
+  //return last element;
+  return last;
+
+
+
+};
+
+Stack.prototype.size = function() {
+  return this.length;
+};
+
+
 
 
