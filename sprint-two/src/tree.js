48abd8b758fe61509ea3294af042470670e9de89
asutedja <asutedja@gmail.com>
2016-07-28 19:27:35 -0700

    Almost finish tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,20 +2,46 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
+  extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
+};
+
+
+var extend = function(to, here) {
+  for (var key in here) {
+    to[key] = here[key];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
   // your code here
-  newTree.children = null;  // fix me
+  this.value = value;
+  this.children.push(this);
+
+  console.log(this);
+   // fix me
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  if (this.children.length < 1) {
+    for (var i = 0; i < this.children.length; i++) {
+      var child = this.children[i]; 
+      if (child.contains(target)) {
+        console.log(this.value);
+        return true;
+      }
+    }
+  }
+
+  return false;
 };
 
 
