cad2e6e555f70e9a3008464bfc5622b8f97b7304
asutedja <asutedja@gmail.com>
2016-07-29 20:25:14 -0700

    Begin doubleLinkedList data structure

---
+++ sprint-two/src/doubleLinkedList.js
@@ -0,0 +1,36 @@
+var doubleLinkedList = function() {
+  var nodes = {};
+
+  nodes.addToTail = function(value) {
+
+  };
+
+  nodes.removeHead = function() {
+
+  };
+
+  nodes.contain = function(value) {
+
+  };
+
+  nodes.addToHead = function(value) {
+
+  };
+
+  nodes.removeTail - function() {
+
+  };
+
+  
+};
+
+
+var Node = function(value) {
+  var node = {};
+  node.head = null;
+  node.tail = null;
+  node.value = value;
+  node.next = null;
+  node.previous = null;
+  return node;
+};
